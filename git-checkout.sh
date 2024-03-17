b9357b47cdc963120ff8078f50c8a6ffc33f2d00
Miguel Kolomeitsev <michael.kolomeitsev@tempest.com>
2024-03-17 03:42:54 +0100

    Make git-checkout.sh POSIX compliant and Win-awared

--- git-checkout.sh
+++ git-checkout.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -7,19 +7,27 @@ usage() {
 	exit 1
 }
 
+abs_path() {
+	if [ "$MSYSTEM" ]; then
+		cd "$1" && pwd -W
+	else
+		cd "$1" && pwd
+	fi
+}
+
 set_git_cfg() {
-	if [[ $URL == https://* ]] && [[ $GITHUB_TOKEN ]]; then
-		local CREDS=$(echo -n "x-access-token:$GITHUB_TOKEN" | base64)
+	if [ "$URL" = "https://${URL#https://}" ] && [ "$GITHUB_TOKEN" ]; then
+		CREDS=$(echo -n "x-access-token:$GITHUB_TOKEN" | base64)
 		git config http.extraHeader "Authorization: basic $CREDS"
 	fi
 }
 
 guess_repo() {
-	local GURL=$(git config --get remote.origin.url)
-	local GREPO=${GURL#git@github.com:}
+	GURL=$(git config --get remote.origin.url)
+	GREPO=${GURL#git@github.com:}
 	GREPO=${GREPO#https://github.com/}
 	if [ "$REPO" != "$GREPO" ]; then
-		[[ $REPO ]] && echo "Error: repo mismatch $REPO vs $GREPO ($URL vs $GURL)" && exit 1
+		[ "$REPO" ] && echo "Error: repo mismatch $REPO vs $GREPO ($URL vs $GURL)" && exit 1
 		REPO=$GREPO
 		URL=$GURL
 	fi
@@ -30,74 +38,75 @@ clone_ref_repo() {
 	[ -z "$URL" ] && echo Error: repo not defined && usage
 	[ -z "$REF_DIR" ] && echo Error: reference dir required to clone && usage
 	mkdir -p "$REF_DIR"
-	pushd "$REF_DIR"
+	cd "$REF_DIR"
 	git init --bare
 	git remote add origin "$URL"
 	set_git_cfg
 	git fetch --prune --prune-tags --tags
-	popd > /dev/null
+	cd - > /dev/null
 }
 
 update_ref_repo() {
 	[ -z "$REF_DIR" ] && echo Error: reference dir required to update && usage
-	pushd "$REF_DIR"
+	cd "$REF_DIR"
 	guess_repo
 	git fetch --prune --prune-tags --tags
-	popd > /dev/null
+	cd - > /dev/null
 }
 
 clone_target_repo() {
 	[ -z "$URL" ] && echo Error: repo not defined && usage
 	[ -z "$REF_DIR" ] && echo Error: reference dir required to clone && usage
 	[ -z "$TARGET_DIR" ] && echo Error: target dir required to clone && usage
-	ABS_REF_DIR=$(realpath "$REF_DIR")
+	ABS_REF_DIR=$(abs_path "$REF_DIR")
 	mkdir -p "$TARGET_DIR"
-	pushd "$TARGET_DIR"
+	cd "$TARGET_DIR"
 	git init
 	git remote add origin "$URL"
 	set_git_cfg
 	echo "$ABS_REF_DIR"/objects > .git/objects/info/alternates
 	git fetch --prune --prune-tags --tags
-	popd > /dev/null
+	cd - > /dev/null
 }
 
 update_target_repo() {
 	[ -z "$TARGET_DIR" ] && echo Error: target dir required to update && usage
-	[[ $REF_DIR ]] && ABS_REF_DIR=$(realpath "$REF_DIR")
-	pushd "$TARGET_DIR"
+	[ "$REF_DIR" ] && ABS_REF_DIR=$(abs_path "$REF_DIR")
+	SAVPWD=$PWD
+	cd "$TARGET_DIR"
 	guess_repo
 	if [ -s .git/objects/info/alternates ]; then
 		GREF_DIR=$(dirname `cat .git/objects/info/alternates`)
 		if [ "$ABS_REF_DIR" != "$GREF_DIR" ]; then
-			[[ $REF_DIR ]] && echo Error: ref-dir mismatch $ABS_REF_DIR vs $GREF_DIR && exit 1
+			[ "$REF_DIR" ] && echo Error: ref-dir mismatch $ABS_REF_DIR vs $GREF_DIR && exit 1
 			REF_DIR=$GREF_DIR
 			update_ref_repo
 		fi
 	fi
 	git fetch --prune --prune-tags --tags
-	popd > /dev/null
+	cd "$SAVPWD"
 }
 
 clean() {
 	[ -z "$TARGET_DIR" ] && echo Error: target dir required to clean && usage
-	pushd "$TARGET_DIR"
+	cd "$TARGET_DIR"
 	git clean -dffx
 	git reset --hard HEAD
 	git submodule deinit --force --all
-	[[ $(git status --porcelain) ]] && echo Clean failed && exit 1
-	popd > /dev/null
+	[ "$(git status --porcelain)" ] && echo Clean failed && exit 1
+	cd - > /dev/null
 }
 
 checkout() {
 	[ -z "$TARGET_DIR" ] && echo Error: target dir required to checkout && usage
 	[ -z "$TARGET_REF" ] && echo Error: target ref required to checkout && usage
-	pushd "$TARGET_DIR"
-	if [[ $(git branch --remotes --list origin/$TARGET_REF) ]]; then
+	cd "$TARGET_DIR"
+	if [ "$(git branch --remotes --list origin/$TARGET_REF)" ]; then
 		git checkout -B $TARGET_REF origin/$TARGET_REF
 	else
 		git checkout $TARGET_REF
 	fi
-	popd > /dev/null
+	cd - > /dev/null
 }
 
 
@@ -108,7 +117,7 @@ REF_DIR=
 TARGET_DIR=
 TARGET_REF=
 CLEAN=
-while [[ $# -gt 0 ]]; do
+while [ $# -gt 0 ]; do
 	case "$1" in
 		--repo)
 			[ -z "$2" ] && echo Error: --repo requires an argument && usage
@@ -148,9 +157,9 @@ while [[ $# -gt 0 ]]; do
 	esac
 done
 
-if [[ $REPO ]]; then
-	[[ ! $REPO == *.git ]] && REPO=$REPO.git
-	if [[ $GITHUB_TOKEN ]]; then
+if [ "$REPO" ]; then
+	[ "$REPO" = "${REPO%.git}" ] && REPO=$REPO.git
+	if [ "$GITHUB_TOKEN" ]; then
 		URL=https://github.com/$REPO
 	else
 		URL=git@github.com:$REPO
@@ -173,8 +182,8 @@ else
 	clone_target_repo
 fi
 
-[[ $CLEAN ]] && clean
+[ "$CLEAN" ] && clean
 
-[[ $TARGET_REF ]] && checkout
+[ "$TARGET_REF" ] && checkout
 
 exit 0
